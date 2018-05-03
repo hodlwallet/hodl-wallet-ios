f30e66dc4baf5490e82193b88a4740210d1f2571
Edgar Perez <eaprmail@gmail.com>
2018-05-03 14:52:02 -0700

    Update Font

--- breadwallet/src/Extensions/UIFont+BRWAdditions.swift
+++ breadwallet/src/Extensions/UIFont+BRWAdditions.swift
@@ -10,19 +10,19 @@ import UIKit
 
 extension UIFont {
     static var header: UIFont {
-        guard let font = UIFont(name: "CircularPro-Bold", size: 17.0) else { return UIFont.preferredFont(forTextStyle: .headline) }
+        guard let font = UIFont(name: "Electrolize-Regular", size: 17.0) else { return UIFont.preferredFont(forTextStyle: .headline) }
         return font
     }
     static func customBold(size: CGFloat) -> UIFont {
-        guard let font = UIFont(name: "CircularPro-Bold", size: size) else { return UIFont.preferredFont(forTextStyle: .headline) }
+        guard let font = UIFont(name: "Electrolize-Regular", size: size) else { return UIFont.preferredFont(forTextStyle: .headline) }
         return font
     }
     static func customBody(size: CGFloat) -> UIFont {
-        guard let font = UIFont(name: "CircularPro-Book", size: size) else { return UIFont.preferredFont(forTextStyle: .subheadline) }
+        guard let font = UIFont(name: "Electrolize-Regular", size: size) else { return UIFont.preferredFont(forTextStyle: .subheadline) }
         return font
     }
     static func customMedium(size: CGFloat) -> UIFont {
-        guard let font = UIFont(name: "CircularPro-Medium", size: size) else { return UIFont.preferredFont(forTextStyle: .body) }
+        guard let font = UIFont(name: "Electrolize-Regular", size: size) else { return UIFont.preferredFont(forTextStyle: .body) }
         return font
     }
 
