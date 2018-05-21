32f317795285cc5943658feb96025e040fd301bc
Edgar Perez <eaprmail@gmail.com>
2018-05-21 10:44:42 -0400

    Update Touch ID View

--- breadwallet/src/Views/PhraseView.swift
+++ breadwallet/src/Views/PhraseView.swift
@@ -29,7 +29,7 @@ class PhraseView: UIView {
         label.textColor = .white
         label.text = phrase
         label.font = UIFont.customBold(size: 16.0)
-        backgroundColor = .black
+        backgroundColor = .darkGray
         // layer.cornerRadius = 10.0
     }
 
