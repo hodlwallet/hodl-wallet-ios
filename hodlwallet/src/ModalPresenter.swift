cca0639d004e171cb66e9c1f50bbb971eaf0751d
arubinofaux <arubinofaux@gmail.com>
2018-05-30 17:03:34 -0400

    remove share anonymous data settings

--- hodlwallet/src/ModalPresenter.swift
+++ hodlwallet/src/ModalPresenter.swift
@@ -417,9 +417,7 @@ class ModalPresenter : Subscriber, Trackable {
                 })
             ],
             "Bread": [
-                Setting(title: S.Settings.shareData, callback: {
-                    settingsNav.pushViewController(ShareDataViewController(store: self.store), animated: true)
-                }),
+                
                 Setting(title: S.Settings.about, callback: {
                     settingsNav.pushViewController(AboutViewController(), animated: true)
                 }),
